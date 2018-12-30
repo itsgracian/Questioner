247b898c85513ec4154e193dabae974505008ca4
gracian <gracian2020@gmail.com>
2018-12-31 01:30:08 +0200

    response.js in validation folder modified

--- validations/response.js
+++ validations/response.js
@@ -3,14 +3,9 @@ const isEmpty = require("./isEmpty");
 
 module.exports = (data) => {
   const errors = {};
-  data.meetup = !isEmpty(data.meetup) ? data.meetup : "";
   data.status = !isEmpty(data.status) ? data.status : "";
   data.topic = !isEmpty(data.topic) ? data.topic : "";
   //@validator
-  if (validator.isEmpty(data.meetup)) {
-    errors.meetup = "meetup field is required";
-  }
-  //
   if (validator.isEmpty(data.status)) {
     errors.status = "status field is required";
   }
