881bd71ba03622884416dee9a95b6c68914b0219
gracian <gracian2020@gmail.com>
2018-12-30 22:29:48 +0200

    response.js modified

--- validations/response.js
+++ validations/response.js
@@ -4,19 +4,19 @@ const isEmpty = require("./isEmpty");
 module.exports = (data) => {
   const errors = {};
   data.meetup = !isEmpty(data.meetup) ? data.meetup : "";
-  data.status=!isEmpty(data.status) ? data.status :"";
-  data.topic=!isEmpty(data.topic) ? data.topic :"";
+  data.status = !isEmpty(data.status) ? data.status : "";
+  data.topic = !isEmpty(data.topic) ? data.topic : "";
   //@validator
   if (validator.isEmpty(data.meetup)) {
-    errors.meetup='meetup field is required';
+    errors.meetup = "meetup field is required";
   }
   //
   if (validator.isEmpty(data.status)) {
-    errors.status="status field is required";
+    errors.status = "status field is required";
   }
   //
   if (validator.isEmpty(data.topic)) {
-    errors.topic="topic field is required";
+    errors.topic = "topic field is required";
   }
   return {
     errors,
