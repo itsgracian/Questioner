36870a2c093619fa97fb943ee1856d96d8eadfab
Itsgracian <gracian2020@gmail.oom>
2019-03-04 18:17:09 +0200

    database connection modified

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,21 +2,20 @@ import { Pool } from "pg";
 import config from "./keys";
 import dotenv from "dotenv";
 dotenv.config();
-let pool;
+// let pool;
 //@
-if(process.env.NODE_ENV==="DEV"){
+//if(process.env.NODE_ENV==="DEV"){
 //@database connection
-pool = new Pool({
+const pool = new Pool({
  connectionString:process.env.DATABASE_URL
 });
-}
-console.log(process.env.DATABASE_URL);
-//@this runs when someone is testing app using TDD
-if(process.env.NODE_ENV==="TEST"){
-  //@database connection
-   pool = new Pool({
-   connectionString:config.dbTestURI
-  });
+//}
+// //@this runs when someone is testing app using TDD
+// if(process.env.NODE_ENV==="TEST"){
+//   //@database connection
+//    pool = new Pool({
+//    connectionString:config.dbTestURI
+//   });
   }
   export default pool;
 
