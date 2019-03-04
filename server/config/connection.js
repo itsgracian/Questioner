8c976073709863265cb50cc9cdbcb19378257904
Itsgracian <gracian2020@gmail.oom>
2019-03-04 12:37:41 +0200

    unnecessary files removed

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,16 +3,15 @@ import config from "./keys";
 import dotenv from "dotenv";
 dotenv.config();
 let pool;
-//@first we have to check if we are making test or we are in developing
+//@
 if(process.env.NODE_ENV==="DEV"){
 //@database connection
 console.log(config.databaseURI);
 pool = new Pool({
-  //connectionString: process.env.DATABASE_URL
  connectionString:config.databaseURI
 });
 }
-console.log(config.dbTestURI);
+//@this runs when someone is testing app using TDD
 if(process.env.NODE_ENV==="TEST"){
   //@database connection
    pool = new Pool({
